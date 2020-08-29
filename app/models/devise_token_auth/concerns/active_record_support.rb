eceacb96b32b903124763d837f151c57f7236e8b
Dan Loman <daniel.h.loman@gmail.com>
2020-08-29 09:47:55 -0400

    Include TokensSerialization inline

--- app/models/devise_token_auth/concerns/active_record_support.rb
+++ app/models/devise_token_auth/concerns/active_record_support.rb
@@ -1,16 +1,34 @@
-require_relative 'tokens_serialization'
-
 module DeviseTokenAuth::Concerns::ActiveRecordSupport
   extend ActiveSupport::Concern
 
   included do
-    serialize :tokens, DeviseTokenAuth::Concerns::TokensSerialization
+    serialize :tokens, TokensSerialization
   end
 
   class_methods do
     # It's abstract replacement .find_by
     def dta_find_by(attrs = {})
       find_by(attrs)
+    end
+  end
+end
+
+class TokensSerialization
+  # Serialization hash to json
+  def self.dump(object)
+    object.each_value(&:compact!) unless object.nil?
+    JSON.generate(object)
+  end
+
+  # Deserialization json to hash
+  def self.load(json)
+    case json
+    when String
+      JSON.parse(json)
+    when NilClass
+      {}
+    else
+      json
     end
   end
 end
